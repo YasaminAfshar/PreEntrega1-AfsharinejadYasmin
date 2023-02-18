16726a9f60eac107872157eba1fc35b771d26b0c
Yasmin Afsharinejad <iasaminblack@gmail.com>
2023-02-18 16:03:21 -0300

    PreEntrega1-AfsharinejadYasmin

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+
+//Importar los subcomponentes que se aplica al Navbar
+
+
+//Importar el arhivo de CSS
+import style from "./Navbar.module.css";
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className={style.titleForm}>Hola mundo!</h1>
+        </div>
+    );
+};
+
+export default Navbar;
