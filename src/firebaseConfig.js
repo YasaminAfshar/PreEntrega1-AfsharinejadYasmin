a7306d0d3776572914179657d71549d6229311bb
Yasmin Afsharinejad <iasaminblack@gmail.com>
2023-03-28 00:21:23 -0300

    variable de entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBgeXO8kuka7DI1OXdCKQy9BPPTHscJjnc",
-  authDomain: "mundolana-18397.firebaseapp.com",
-  projectId: "mundolana-18397",
-  storageBucket: "mundolana-18397.appspot.com",
-  messagingSenderId: "1091404851002",
-  appId: "1:1091404851002:web:6b9f895f77c7124be11203"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROYECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
